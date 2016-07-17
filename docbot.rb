a478330348ad6ea1ab4950e804cd60554de9c10f
apmenard1993 <apmenard1993@gmail.com>
2016-07-16 19:06:12 -0700

    adding db and trying message grabbing

--- docbot.rb
+++ docbot.rb
@@ -2,7 +2,7 @@ require 'discordrb'
 require './markovchat'
 
 bot = Discordrb::Bot.new token: 'MjAxOTIzMDg0NTc4NTg2NjMz.CmS4vw.fnv8LQOetPNgS684ZzKeSWNzU7U', application_id: 201910521111379968
-m = MarkovChat.new
+m = MarkovChat.new("docChat.db")
 
 puts "This bot's invite URL is #{bot.invite_url}."
 puts 'Click on it to invite it to your server.'
@@ -12,7 +12,11 @@ bot.message(with_text: 'Ping!') do |event|
     msg.edit "Pong! Time taken: #{Time.now - event.timestamp} seconds."
 end
 
-bot.message(with_text: 'pls', in: "#docs") do |event|
+counter = 0
+
+bot.message(in: "#docs") do |event|
+    content = event.content
+    puts content
     msg = event.respond 'NO'
 end
 
